759c6185bc5b35e9fc0e6de9e0f85a0a2d9de825
Slyce20 <doxxing478@gmail.com>
2026-05-02 02:19:17 +0200

    Update menu-download.js

--- plugins/menu-download.js
+++ plugins/menu-download.js
@@ -23,7 +23,7 @@ const defaultMenu = {
   header: '┍━━━〔 %category 〕━━━┑',
   body: '┇ 📥  *%cmd*',
   footer: '┕━━━━━──ׄ──ׅ──ׄ──━━━━━┙\n',
-  after: `_ʙʟᴅ-ʙᴏᴛ ɴᴇᴛᴡᴏʀᴋ ᴅᴀᴛᴀ_`
+  after: `ˢᴸʸᶜᴱ ɴᴇᴛᴡᴏʀᴋ ᴅᴀᴛᴀ_`
 }
 
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
